2b5cee00a25c2e3a26c15a085e53595719206c03
aguiartalita <aguiar.talita@outlook.com>
2022-09-29 21:58:59 -0300

    incluido interatividade de adicionar tarefa

--- README.md
+++ README.md
@@ -5,21 +5,22 @@ das skills técnicas.
 
 ## Aprendizagem
 
-React 
-Typescript
-Styled component
-FontAwesome icons
-Interface Props
-Event onChange
+- React 
+- Typescript
+- Styled component
+- FontAwesome icons
+- Interface Props
+- Event onChange
 
 
-## Este projeto roda
+## Ambiente de desenvolvimento
 ### `npm start`
 
-Ambiente de desenvolvimento [http://localhost:3000](http://localhost:3000) to view it in the browser.
+Ambiente de desenvolvimento [http://localhost:3000](http://localhost:3000) para acessar no navegador.
 
 A página será recarregada se você fizer edições.\
 Você também verá erros de lint no console.
+<!-- infos react base create -->
 
 ## Código de origem/base 
 [React com Typescript Youtube](https://www.youtube.com/watch?v=95sAtAareR8)
--- src/components/AddArea/index.tsx
+++ src/components/AddArea/index.tsx
@@ -1,19 +1,39 @@
-import * as C from './styles'
+import { useState, KeyboardEvent } from 'react';
+import * as C from './styles';
 import { faPlusCircle } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-export const AddArea = () => {
-  return (
-    <C.Container>
-      <div className='icon'>
-        <FontAwesomeIcon 
-        icon={faPlusCircle}
-        font-size={'2rem'}
-         /> 
-      </div>
-      <input 
-      type="text"
-      placeholder='Adicione uma tarefa'
-      />
-    </C.Container>
-  )
+
+interface Props  {
+    onEnter: (taskName: string) => void
+}
+
+export const AddArea = ({ onEnter }: Props) => {
+    const [inputText, setInputText] = useState('');
+
+    const handleKeyUp = (e: KeyboardEvent) => {
+        if(e.code === 'Enter' && inputText !== '') {
+            onEnter(inputText);
+            setInputText('');
+        }
+    }
+
+    return (
+        <C.Container>
+            <div 
+              className='icon'
+              >
+                <FontAwesomeIcon 
+                icon={faPlusCircle}
+                fontSize={'2rem'}
+                /> 
+           </div>
+            <input
+                type="text"
+                placeholder="Adicione uma tarefa"
+                value={inputText}
+                onChange={e=>setInputText(e.target.value)}
+                onKeyUp={handleKeyUp}
+            />
+        </C.Container>
+    );
 }
